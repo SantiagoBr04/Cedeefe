b5a1368e64773d678069f30e1da4c71d7be4ffd3
Karen Santos <emailkarenescola@gmail.com>
2025-09-18 12:11:51 -0300

    sidebar

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+ /*BOTÃO MENU*/
+ const btnMenu = document.querySelector('#btn-menu');
+ const sidebar = document.querySelector('.sidebar', );
+
+ btnMenu.addEventListener('click', ()=>{
+    sidebar.classList.toggle('expandido');
+ })
