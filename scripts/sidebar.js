96131d744616fc1563ec64387386e91a898c820c
Karen Santos <emailkarenescola@gmail.com>
2025-09-27 21:28:42 -0300

    Página Criar Lista

--- scripts/sidebar.js
+++
@@ -1,7 +0,0 @@
- /*BOTÃO MENU*/
- const btnMenu = document.querySelector('#btn-menu');
- const sidebar = document.querySelector('.sidebar', );
-
- btnMenu.addEventListener('click', ()=>{
-    sidebar.classList.toggle('expandido');
- })
